306a9921860d2aa0f65e2d74993d711a862bc3b5
kim-kiwon <kimkiwonn@gmail.com>
2022-10-23 23:43:17 +0900

    test: String 클래스 split, substring, charat 테스트 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,43 @@
 package study;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("String.split 테스트 수행.")
+    void split() {
+        String[] values = "1,2".split(",");
+        assertThat(values).containsExactly("1", "2");
+
+        values = "1".split(",");
+        assertThat(values).contains("1");
+    }
+
+    @Test
+    @DisplayName("String.substring 테스트 수행.")
+    void substring() {
+        String value = "(1,2)".substring(1,4);
+        assertThat(value).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 테스트 수행. 인덱스를 벗어나면 StringIndexOutOfBoundsException 에러를 던진다.")
+    void charAt() {
+        String value = "abc";
+        int index = 1;
+        char result = value.charAt(index);
+        assertThat(result).isEqualTo('b');
+
+        assertThatThrownBy(() -> "abc".charAt(10))
+            .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
